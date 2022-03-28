e8f11118d191ca68dccb49ee251adc6e292869fb
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-28 07:20:56 +0300

    It adds User models and displays the images from admin database

--- photolab/views.py
+++ photolab/views.py
@@ -8,4 +8,13 @@ from .models import Image, Location, User, Category
 def welcome(request):
     return render(request, 'welcome.html')
 
+def photolab(request):
+    # imports photos and save it in database
+    location = Location.objects.all()
+    photolab = Image.objects.all()
+    date=dt.date.today()
+    users=User.objects.all()
+    category=Category.objects.all()
+    return render(request, 'all-photos/photolab.html',{'photolab':photolab,'location':location, 'date':date,'category':category,})
+
 
