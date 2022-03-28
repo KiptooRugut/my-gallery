78e73ab5c79c2fc22183f588ca6bd2cb1c839808
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-28 07:17:36 +0300

    it will connect our template with the view function

--- photolab/views.py
+++ photolab/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http import HttpResponse
+import datetime as dt
+
+from .models import Image, Location, User, Category
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
 
