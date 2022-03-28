a369e868f01f5ceed0436a87846e00bf85da7b47
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-28 06:55:35 +0300

    it will pass in the URL regular expression the view and a name keyword argument.

--- photolab/urls.py
+++ photolab/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index'),
+    path('', views.photolab, name='photolab'),
     path('search/', views.search_results, name='search_results'),
     path('photos/(\d+)',views.photo,name ='photo')
 ]
